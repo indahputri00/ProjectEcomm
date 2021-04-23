5620ec9081bfa3c48a3de04ce45c27eab72ff92e
indahputri00 <indahkurniaputri23.ikp@gmail.com>
2021-04-23 08:51:12 +0700

    toko

---
+++ application/views/shopping/sukses.php
@@ -0,0 +1,7 @@
+<h2>Proses Order sukses</h2>
+<div class="kotak2">
+<p>Terima kasih sudah berbelanja HIJAB-Ku !</p>
+<p>Barang anda akan segera di proses. Jika dalam 3x24jam barang anda belum di kemas, silahkan hubungi costumer service kami.</p>
+<br>
+<p> CP : 020-8432934234 </p>
+</div>
