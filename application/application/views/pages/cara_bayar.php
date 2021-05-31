21535471c8c986aa07dab24663ad4a3163fd0c0f
indahputri00 <indahkurniaputri23.ikp@gmail.com>
2021-05-31 20:21:28 +0700

    Praktikum10

---
+++ application/application/views/pages/cara_bayar.php
@@ -0,0 +1,12 @@
+<h3>Cara Bayar</h3>
+<p>Dari antara sekian banyak jenis transaksi pembayaran :</p>
+
+<p>1. Pembayaran Kartu Kredit/Visa</p>
+
+<p>2. Transfer Bank</p>
+
+<p>3. Debit Visa</p>
+
+<p>4. Cash On Delivery (COD)</p>
+
+<p>5. E-Wallet</p>
